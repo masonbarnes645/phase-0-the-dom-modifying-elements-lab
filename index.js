4ec8906a61de3587b9e76766c1179764db93b788
Mason Barnes <barnesmason980@gmail.com>
2024-05-09 13:05:05 -0700

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main2 = document.getElementById('main');
+document.body.remove(main2);
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent = ('Mason is the champion');
+document.body.append(newHeader);
